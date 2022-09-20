55aa24eb7e436a36d83a1773c6d52ad2df5676e3
Magnus Møller <magnus108@me.com>
2022-09-20 19:00:49 +0200

    darwin

--- flake.nix
+++ flake.nix
@@ -3,24 +3,74 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    yabai-src = {
+      url = "github:koekeishiya/yabai";
+      flake = false;
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations.magnus = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [
-          ./home.nix
-        ];
-
+  outputs = { self, darwin, nixpkgs, home-manager, ...}@inputs:
+  let
+    configuration = { pkgs, ... }: {
+      system.stateVersion = 4;
+      services.nix-daemon.enable = true;
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      programs = { 
+	    zsh.enable = true;
       };
+      nix.package = pkgs.nixFlakes;
+      users.users.magnus = { 
+	    home = "/Users/magnus";
+	    shell = pkgs.zsh;
+      };
+      services.yabai.enable = true;
+      services.skhd.enable = true;
+
+      environment.systemPackages = [];
     };
+  in
+  {
+    darwinConfigurations.magnus = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ({ config, pkgs, lib, ... }: {
+          nixpkgs = {
+            overlays = with inputs; [
+                (final: prev: {
+                  yabai =
+                    let
+                      version = "4.0.0-dev";
+                      buildSymlinks = prev.runCommand "build-symlinks" { } ''
+                        mkdir -p $out/bin
+                        ln -s /usr/bin/xcrun /usr/bin/xcodebuild /usr/bin/tiffutil /usr/bin/qlmanage $out/bin
+                      '';
+                    in
+                    prev.yabai.overrideAttrs (old: {
+                      inherit version;
+                      src = inputs.yabai-src;
+
+                      buildInputs = with prev.darwin.apple_sdk.frameworks; [
+                        Carbon
+                        Cocoa
+                        ScriptingBridge
+                        prev.xxd
+                        SkyLight
+                      ];
+
+                      nativeBuildInputs = [ buildSymlinks ];
+                    });
+                })];
+            };
+            })
+          configuration home-manager.darwinModules.home-manager {
+				home-manager.users.magnus = import ./home.nix;
+      	  }
+       ];
+    };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,118 +1,108 @@
 { pkgs, ... }: {
-  home.stateVersion = "21.11";
-  home.username = "magnus";
-  home.homeDirectory = "/Users/magnus";
 
-  programs.home-manager.enable = true;
+    home.stateVersion = "21.11";
 
-  home.packages = with pkgs; [
-    ranger
-    rsync
-    zip
-    unzip
-    htop
-    wget
-    curl
-    mupdf
-    tig
-    llvm
-    yabai
-    font-awesome
-    spacebar
-    skhd
-  ];
+    home.username = "magnus";
+    home.homeDirectory = "/Users/magnus";
 
-  xdg.configFile."tig/config".text = builtins.readFile ./tigrc;
+    programs.home-manager.enable = true;
 
-  xdg.configFile."nvim/coc-settings.json".text = builtins.readFile ./coc-settings.json;
+    xdg.configFile."tig/config".text = builtins.readFile ./tigrc;
+    xdg.configFile."nvim/coc-settings.json".text = builtins.readFile ./coc-settings.json;
+    
+    home.packages = with pkgs; [
+	    ripgrep
+	    rsync
+	    zip
+	    unzip
+	    wget
+	    curl
+	    tig
+    ];
 
-  programs.noti = {
-    enable = true;
-  };
-
-  programs.navi = {
-    enable = true;
-  };
-
-  programs.kitty = {
-    enable = true;
-    theme = "Gruvbox Dark";
-  };
-
-  programs.neovim = {
-    enable = true;
-    extraConfig = builtins.readFile ./vimrc.vim;
-    viAlias = true;
-    vimAlias = true;
-    withNodeJs = true;
-    withPython3 = true;
-    plugins = with pkgs.vimPlugins; [ haskell-vim coc-nvim ];
-  };
-
-  programs.tmux = {
-    enable = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    history = {
-      size = 102400;
-      save = 102400;
-      ignoreDups = true;
-      expireDuplicatesFirst = true;
-      share = true;
-    };
-    enableSyntaxHighlighting = true;
-    enableAutosuggestions = true;
-    oh-my-zsh = {
-      enable = true;
-      theme = "ys";
-      plugins = [ "git" "sudo" "tmux" "tig"];
-    };
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "Magnus Møller";
-    userEmail = "magnus108@me.com";
-    lfs = {
+    programs.noti = {
       enable = true;
     };
-    extraConfig = {
-      core = {
-        autocrlf = "input";
+
+    programs.navi = {
+      enable = true;
+    };
+
+    programs.kitty = {
+      enable = true;
+      theme = "Gruvbox Dark";
+    };
+
+    programs.neovim = {
+      enable = true;
+      extraConfig = builtins.readFile ./vimrc.vim;
+      viAlias = true;
+      vimAlias = true;
+      withNodeJs = true;
+      withPython3 = true;
+      plugins = with pkgs.vimPlugins; [ haskell-vim coc-nvim ];
+    };
+
+    programs.zsh = {
+      enable = true;
+      history = {
+        size = 102400;
+        save = 102400;
+        ignoreDups = true;
+        expireDuplicatesFirst = true;
+        share = true;
       };
-      color = {
-        ui = "auto";
+      enableSyntaxHighlighting = true;
+      enableAutosuggestions = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "ys";
+        plugins = [ "git" "sudo" "tig"];
       };
-      branch = {
-        autoSetupRebase = "always";
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Magnus Møller";
+      userEmail = "magnus108@me.com";
+      lfs = {
+        enable = true;
       };
-      push = {
-        default = "simple";
-      };
-      pull = {
-        rebase = "merges";
-      };
-      merge = {
-        tool = "vimdiff";
-        conflictStyle = "diff3";
-      };
-      rerere = {
-        enabled = true;
-      };
-      diff = {
-        tool = "vimdiff";
-        mnemonicprefix = true;
-      };
-      rebase = {
-        abbreviateCommands = true;
-      };
-      mergetool = {
-        vimdiff = {
-          cmd = "nvim -d $LOCAL $MERGED $BASE $REMOTE -c 'wincmd w' -c 'wincmd j'";
+      extraConfig = {
+        core = {
+          autocrlf = "input";
+        };
+        color = {
+          ui = "auto";
+        };
+        branch = {
+          autoSetupRebase = "always";
+        };
+        push = {
+          default = "simple";
+        };
+        pull = {
+          rebase = "merges";
+        };
+        merge = {
+          tool = "vimdiff";
+          conflictStyle = "diff3";
+        };
+        rerere = {
+          enabled = true;
+        };
+        diff = {
+          tool = "vimdiff";
+          mnemonicprefix = true;
+        };
+        rebase = {
+          abbreviateCommands = true;
+        };
+        mergetool = {
+          vimdiff = {
+            cmd = "nvim -d $LOCAL $MERGED $BASE $REMOTE -c 'wincmd w' -c 'wincmd j'";
+          };
         };
       };
-    };
   };
 }
