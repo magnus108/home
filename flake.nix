8f696763214f4630ca2d972c69609ba8ef5dc041
Magnus Møller <magnus108@me.com>
2022-09-22 20:52:09 +0200

    fucku2

--- flake.nix
+++ flake.nix
@@ -37,34 +37,34 @@
                             users.magnus = import ./home.nix;
                         };
                     }
-                ({ pkgs, ... }: {
-                    nixpkgs = {
-                        overlays = with inputs; [
-                            spacebar.overlay.aarch64-darwin
-                            (final: prev: {
-                                yabai = prev.yabai.overrideAttrs (old: {
-                                    version = "4.0.0-dev";
-                                    src = yabai-src;
+                    ({ pkgs, ... }: {
+                        nixpkgs = {
+                            overlays = with inputs; [
+                                spacebar.overlay.aarch64-darwin
+                                (final: prev: {
+                                    yabai = prev.yabai.overrideAttrs (old: {
+                                        version = "4.0.0-dev";
+                                        src = yabai-src;
 
-                                    buildInputs = with prev.darwin.apple_sdk.frameworks; [
-                                        Carbon
-                                        Cocoa
-                                        ScriptingBridge
-                                        prev.xxd
-                                        SkyLight
-                                    ];
+                                        buildInputs = with prev.darwin.apple_sdk.frameworks; [
+                                            Carbon
+                                            Cocoa
+                                            ScriptingBridge
+                                            prev.xxd
+                                            SkyLight
+                                        ];
 
-                                    nativeBuildInputs = [ 
-                                        (prev.runCommand "build-symlinks" { } ''
-                                            mkdir -p $out/bin
-                                            ln -s /usr/bin/xcrun /usr/bin/xcodebuild /usr/bin/tiffutil /usr/bin/qlmanage $out/bin
-                                        '')
-                                    ];
-                                });
-                            })
-                        ];
-                    };
-                })
+                                        nativeBuildInputs = [ 
+                                            (prev.runCommand "build-symlinks" { } ''
+                                                mkdir -p $out/bin
+                                                ln -s /usr/bin/xcrun /usr/bin/xcodebuild /usr/bin/tiffutil /usr/bin/qlmanage $out/bin
+                                            '')
+                                        ];
+                                    });
+                                })
+                            ];
+                        };
+                    })
                 ];
             };
         };
