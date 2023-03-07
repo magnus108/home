8a50b7e83e76e57a98af571f23cc25fe5002d5e4
Magnus Møller <magnus108@me.com>
2023-03-07 16:12:18 +0100

    jaja

--- darwin.nix
+++ darwin.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
 
+    nix.settings = {
+        substituters = ["https://cache.iog.io"];
+        trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    };
+    
     programs.zsh.enable = true;
 
     environment.variables = {
